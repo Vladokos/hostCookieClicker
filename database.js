fea9a5b520bffe4f4496a506abcff55cf6503f26
Vlad <66181660+Vladokos@users.noreply.github.com>
2023-01-04 01:44:14 +0400

    added pool queries to stable work

--- database.js
+++ database.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql");
 
-const database = mysql.createConnection({
-  host: "localhost",
+const database = mysql.createPool({
+  connectionLimit: 10,
+  waitForConnections : true,
+  host: "sql9.freesqldatabase.com",
   port: 3306,
-  user: "root",
-  password: "root",
-  database: "cookieclicker",
+  user: "sql9588049",
+  password: "TjC45NjPtq",
+  database: "sql9588049",
 });
 
 
